2b9ab0ff9a6f40a1ac4dacbc1f2fa432cc379da3
wenfh2020 <wenfh2020@126.com>
2021-08-16 12:40:22 +0800

    add script to sync code.

--- sync_code.sh
+++ sync_code.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
-# copy src from mac to linux.
-SRC=~/go/src/agent
-LAN_DST=root@192.168.0.200:/home/gopath/src
-REMOTE_DST=root@wenfh2020_sgx.com:/home/gopath/src
-DST=$LAN_DST
+# rsync code from mac to linux.
 
 work_path=$(dirname $0)
 cd $work_path
 
-[ $1x == 'remote'x ] && DST=$REMOTE_DST
+src=~/go/src/agent
+dst=root@wenfh2020_sgx.com:/home/gopath/src
+echo "$src --> $dst"
 
-echo "send files to: $DST"
-
-rsync -avz --exclude="*.o" \
-    --exclude=".git" \
-    --exclude=".vscode" \
-    --exclude="*.so" \
-    --exclude="*.a" \
-    --exclude="*.log" \
-    --exclude="config.yml" \
-    --exclude="sync_code.sh" \
-    --exclude="main" \
-    --exclude="client" \
-    $SRC $DST
+# only rsync *.go files.
+rsync -ravz --exclude=".git/" --include="*.go" --include="*/" --exclude="*" $src $dst
